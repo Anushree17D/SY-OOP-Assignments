61ee66a57cb6c7a6bcfa36261ddb1ffdd32abc32
Anushree Dixit <anushreedixit635@gmail.com>
2025-10-31 12:07:25 +0530

    Update Question 1.cpp

--- Assignment #10/Question 1.cpp
+++ Assignment #10/Question 1.cpp
@@ -10,13 +10,13 @@ T sumArray(T arr[], int n) {
 }
 
 int main() {
-    int iArr[10] = {1,2,3,4,5,6,7,8,9,10};
-    float fArr[10] = {1.1,2.2,3.3,4.4,5.5,1.1,2.2,3.3,4.4,5.5};
-    double dArr[10] = {0.5,1.5,2.5,3.5,4.5,0.5,1.5,2.5,3.5,4.5};
+    int int_arr[5] = {1 , 2 , 3 , 4 , 5};
+    float flo_arr[5] = {1.1 , 2.2 , 3.3 , 4.4 , 5.5};
+    double dec_arr[5] = {0.5 , 1.5, 2.5 , 3.5 , 4.5};
 
-    cout << "Sum of Integer array: " << sumArray(iArr, 10) << endl;
-    cout << "Sum of Float array: " << sumArray(fArr, 10) << endl;
-    cout << "Sum of Double array: " << sumArray(dArr, 10) << endl;
+    cout << "Sum of Integer array: " << sumArray(int_arr, 5) << endl;
+    cout << "Sum of Float array: " << sumArray(flo_arr, 5) << endl;
+    cout << "Sum of Double array: " << sumArray(dec_arr, 5) << endl;
 
     return 0;
 }
