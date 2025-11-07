12a1e80073fb933d6ec30ce81fa8452b586a7665
Anushree Dixit <anushreedixit635@gmail.com>
2025-11-07 12:15:26 +0530

    Update With Iterators.cpp

--- Assignment 11/With Iterators.cpp
+++ Assignment 11/With Iterators.cpp
@@ -6,19 +6,22 @@ int main() {
     vector<int> v = {1,2,3,4,5,6,7,8,9,10};
 
     cout << "Initial Vector: " << endl;
-    for (auto i = v.begin(); i != v.end(); ++i) {
-        cout << *i << " " << endl;
+    vector<int>::iterator I; 
+
+    for (I = v.begin(); I != v.end(); ++I) {
+        cout << *I << " " << endl;
     }
 
     cout << "Multiply by 10" << endl;
-    for (auto i = v.begin(); i != v.end(); ++i) {
-        *i = (*i) * 10;
+    for (I = v.begin(); I != v.end(); ++I) {
+        *I = (*I) * 10;  
     }
 
     cout << "New Vector: " << endl;
-    for (auto i = v.begin(); i != v.end(); ++i) {
-        cout << *i << " " << endl;
+    for (I = v.begin(); I != v.end(); ++I) {
+        cout << *I << " " << endl;
     }
 
     return 0;
 }
+
